a7372a0dbf41924f43cb27781631675d882cdd18
Roberto Soares <roberto.tech@gmail.com>
2018-11-04 04:10:18 -0200

    Add extract title function

---
+++ test/extract-title.js
@@ -0,0 +1,12 @@
+const { expect } = require("code");
+const { it, describe } = (exports.lab = require("lab").script());
+
+const extractTitle = require("../lib/extract-title");
+
+describe("extract title", () => {
+  it("returns the first title", async () => {
+    const title = await extractTitle("test/fixtures/simple.md")
+
+    expect(title).to.equal("Hello World!");
+  });
+});
---
+++ test/fixtures/simple.md
@@ -0,0 +1,7 @@
+# Hello World!
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+
+## Heading 2
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
