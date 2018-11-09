0bf1b88fa9449738a1db728b55c9fd2f12936986
Roberto Soares <roberto.tech@gmail.com>
2018-11-08 22:03:33 -0200

    Add folder-index scenario

--- test/write-index.js
+++ test/write-index.js
@@ -9,27 +9,67 @@ const {
 
 const { writeIndex } = require("../lib/writing");
 
-const TEST_FILE = 'test/fixtures/writing.md'
+const TEST_FILE = "test/fixtures/writing.md";
 
 describe("writeIndex", () => {
-  beforeEach(() => {
-    fs.writeFileSync(
-      TEST_FILE,
-      "# H1\nText\n<!-- folder-index-start -->\n* Old Point\n<!-- folder-index-end -->\nText"
-    );
+  describe("when folder index block is present", () => {
+    beforeEach(() => {
+      fs.writeFileSync(
+        TEST_FILE,
+        "# H1\nText\n<!-- folder-index-start -->\n* Old Point\n<!-- folder-index-end -->\nText"
+      );
+    });
+
+    afterEach(() => {
+      fs.unlinkSync(TEST_FILE);
+    });
+
+    it("replaces index on file", () => {
+      writeIndex(TEST_FILE, "* Point 1\n* Point 2");
+
+      const content = fs.readFileSync(TEST_FILE, "utf-8");
+
+      expect(content).to.equal(
+        "# H1\nText\n<!-- folder-index-start -->\n* Point 1\n* Point 2\n<!-- folder-index-end -->\nText"
+      );
+    });
   });
 
-  afterEach(() => {
-    fs.unlinkSync(TEST_FILE);
+  describe("when only the key folder-index is present", () => {
+    beforeEach(() => {
+      fs.writeFileSync(TEST_FILE, "# H1\nText\n<!-- folder-index -->\nText");
+    });
+
+    afterEach(() => {
+      fs.unlinkSync(TEST_FILE);
+    });
+
+    it("replaces index on file", () => {
+      writeIndex(TEST_FILE, "* Point 1\n* Point 2");
+
+      const content = fs.readFileSync(TEST_FILE, "utf-8");
+
+      expect(content).to.equal(
+        "# H1\nText\n<!-- folder-index-start -->\n* Point 1\n* Point 2\n<!-- folder-index-end -->\nText"
+      );
+    });
   });
 
-  it("replaces index on file", () => {
-    writeIndex(TEST_FILE, "* Point 1\n* Point 2");
+  describe("when there is no folder-index", () => {
+    beforeEach(() => {
+      fs.writeFileSync(TEST_FILE, "# H1\nText\nText");
+    });
 
-    const content = fs.readFileSync(TEST_FILE, 'utf-8')
+    afterEach(() => {
+      fs.unlinkSync(TEST_FILE);
+    });
 
-    expect(content).to.equal(
-      "# H1\nText\n<!-- folder-index-start -->\n* Point 1\n* Point 2\n<!-- folder-index-end -->\nText"
-    );
+    it("replaces index on file", () => {
+      writeIndex(TEST_FILE, "* Point 1\n* Point 2");
+
+      const content = fs.readFileSync(TEST_FILE, "utf-8");
+
+      expect(content).to.equal("# H1\nText\nText");
+    });
   });
 });
